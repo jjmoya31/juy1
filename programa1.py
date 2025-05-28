dd5cbdafc6c20783e2230c006ff3198650813875
jjmoya31 <jjmoya@uach.mx>
2025-05-28 17:09:21 -0600

    Update programa1.py

--- programa1.py
+++ programa1.py
@@ -1,1 +1,20 @@
+# Columnas con imagenes
 import streamlit as st
+
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+
+with col3:
+    st.header("An mouse")
+    st.image("https://static.streamlit.io/examples/mouse.jpg")
