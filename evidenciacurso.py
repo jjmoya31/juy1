207460e822e240b8e36eb35e25812dffb152fb88
jjmoya31 <jjmoya@uach.mx>
2025-05-29 17:57:16 -0600

    Update evidenciacurso.py

--- evidenciacurso.py
+++ evidenciacurso.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 st.title("Asistente virtual del curso de Tecnología de la leche")
 
+st.image("https://luisartica.wordpress.com/wp-content/uploads/2020/04/tecnologia-de-leche-ii.jpg?w=259")
+
 
