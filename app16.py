bed2af1d96579997eb3e9da8322bd11b1bd0e63a
jjmoya31 <jjmoya@uach.mx>
2025-05-29 16:08:26 -0600

    Update app16.py

--- app16.py
+++ app16.py
@@ -9,11 +9,11 @@ openai_api_key = st.secrets["api_key"]
 client = OpenAI(api_key=openai_api_key)
 
 prompt = st.chat_input("Ingresa alguna pregunta")
-#if prompt==None:
-#   st.stop()
+if prompt==None:
+   st.stop()
 
-#with st.chat_message("user"):
-#   st.markdown(prompt)
+with st.chat_message("user", avatar = "🦖"):
+   st.markdown(prompt)
 
 # Generate a response using the OpenAI API.
 
