1e68a74506620172f20dea69c07cc333f4ec24bc
jjmoya31 <jjmoya@uach.mx>
2025-05-29 18:39:40 -0600

    Update evidenciacurso.py

--- evidenciacurso.py
+++ evidenciacurso.py
@@ -1,3 +1,5 @@
+st.image("https://lh4.googleusercontent.com/proxy/G5-2LYxrjTbpNJCjU_PWqa6MPNu8tOZVtc_6bhfw-g5QH_LqGYsqLE5GBzY9Yq3MHil-UkEN3jJrt3oPP8vbQbUGbN2goMwZQntMm0xe4B6ZdqH2Cbb6Rri5GgQ31Y73Wue_DKkEIaboIN4")
+
 import streamlit as st
 
 st.title(":green[Asistente virtual del curso de Tecnología de la leche]")
@@ -16,4 +18,4 @@ if nombre:
 
 
 import streamlit as st
-st.title("Contenido 1.Situación Láctea 2.Composición de la leche 3.Propiedades y calidad 4.Microbiología de la leche")
+st.write("Contenido 1.Situación Láctea 2.Composición de la leche 3.Propiedades y calidad 4.Microbiología de la leche")
