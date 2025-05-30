c447bd3331fd436b9944fc7c22889fe455cf47dc
jjmoya31 <jjmoya@uach.mx>
2025-05-29 18:14:36 -0600

    Update evidenciacurso.py

--- evidenciacurso.py
+++ evidenciacurso.py
@@ -4,3 +4,11 @@ st.title(":red[Asistente virtual del curso de Tecnología de la leche]")
 
 st.image("https://luisartica.wordpress.com/wp-content/uploads/2020/04/tecnologia-de-leche-ii.jpg?w=259")
 
+import streamlit as st
+st.title("Ejemplo de entrada de texto")
+
+# Widget: text_input
+nombre = st.text_input("Escribe tu nombre")
+
+if nombre:
+    st.write(f"Hola, {nombre} 👋")
