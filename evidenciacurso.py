cd089ea65fd115a8f88376edb969d5bc2bfd8c7e
jjmoya31 <jjmoya@uach.mx>
2025-05-29 18:12:23 -0600

    Update evidenciacurso.py

--- evidenciacurso.py
+++ evidenciacurso.py
@@ -4,9 +4,3 @@ st.title(":red[Asistente virtual del curso de Tecnología de la leche]")
 
 st.image("https://luisartica.wordpress.com/wp-content/uploads/2020/04/tecnologia-de-leche-ii.jpg?w=259")
 
-# 02_entrada_texto.py
-import streamlit as st
-
-nombre = st.text_input("¿Cuál es tu nombre?")
-if nombre:
-    st.write(f"Hola, {nombre} 👋")
